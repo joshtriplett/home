fa50946375650615259ce3d87eb5c0e57f4dc4ce
Josh Triplett <josh@freedesktop.org>
2008-05-28 23:07:47 -0700

    user.js: Don't try to "fix" URLs by adding www. and .com

--- .mozilla/firefox/2fr6gqfu.default/user.js
+++ .mozilla/firefox/2fr6gqfu.default/user.js
@@ -8,6 +8,9 @@ user_pref("browser.download.manager.retention", 1);
 // Don't shrink images to fit.
 user_pref("browser.enable_automatic_image_resizing", false);
 
+// Don't try to "fix" URLs by adding www. and .com
+user_pref("browser.fixup.alternate.enabled", false);
+
 // Open almost everything in tabs rather than new windows.
 user_pref("browser.link.open_external", 3);
 user_pref("browser.link.open_newwindow", 3);

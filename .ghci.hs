3e446009ede4d77831e4741d3886a4b9b87926cf
Josh Triplett <josh@joshtriplett.org>
2018-09-17 18:56:30 -0700

    Delete old ghci configuration

--- .ghci.hs
+++
@@ -1,8 +0,0 @@
-module GHCI(
-    (#)
-) where
-import Data.Char(intToDigit)
-import Numeric(showIntAtBase)
-(#) :: (Integral a, Show a) => a -> a -> IO ()
-num # base = putStrLn $ showIntAtBase base intToDigit num ""
-infix 0 #

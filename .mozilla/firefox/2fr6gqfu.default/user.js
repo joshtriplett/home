bb4c5225b1955288949251f59eb2a086f947e3da
Josh Triplett <josh@joshtriplett.org>
2019-11-20 20:52:25 -0800

    Firefox: Drop old preferences that either no longer exist or became the default

--- .mozilla/firefox/2fr6gqfu.default/user.js
+++ .mozilla/firefox/2fr6gqfu.default/user.js
@@ -1,9 +1,3 @@
-// Use system DPI.
-user_pref("layout.css.dpi", 0);
-
-// Clear the download manager on exit.
-user_pref("browser.download.manager.retention", 1);
-
 // Don't shrink images to fit.
 user_pref("browser.enable_automatic_image_resizing", false);
 
@@ -12,14 +6,6 @@ user_pref("browser.fixup.alternate.enabled", false);
 
 // Don't remember form entries
 user_pref("browser.formfill.enable", false);
-
-// Open almost everything in tabs rather than new windows.
-user_pref("browser.link.open_external", 3);
-user_pref("browser.link.open_newwindow", 3);
-user_pref("browser.link.open_newwindow.restriction", 2);
-
-// Don't prompt about setting the default browser.
-user_pref("browser.shell.checkDefaultBrowser", false);
 
 // Use blank homepage.
 user_pref("browser.startup.page", 0);
@@ -32,13 +18,6 @@ user_pref("browser.zoom.siteSpecific", false);
 
 // Don't warn about about:config.
 user_pref("general.warnOnAboutConfig", false);
-
-// Don't load a URL from the clipboard when I middle click on content.
-user_pref("middlemouse.contentLoadURL", false);
-
-// Don't suggest downloading plugins.
-user_pref("plugin.default_plugin_disabled", false);
-user_pref("plugins.hide_infobar_for_missing_plugin", true);
 
 // Don't show a message when blocking a popup.
 user_pref("privacy.popups.showBrowserMessage", false);
